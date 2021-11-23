ac9e5aa202cf199a31326275e3ebab8f1700122c
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2021-11-23 09:30:10 -0300

    Iniciando parte de rental

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -18,20 +18,16 @@ class CreateCarSpecificationUseCase {
 
   async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
     const carsExists = await this.carsRepository.findById(car_id);
-    console.log('1: ', carsExists)
 
     if(!carsExists) {
       throw new AppError('Cars does not exits!');
     }
 
     const specifications = await this.specificationsRepository.findByIds(specifications_id)
-    console.log('2: ', specifications)
 
     carsExists.specifications = specifications;
 
     const a = await this.carsRepository.create(carsExists);
-    console.log('A: ', a);
-    console.log('Aqui:', carsExists)
 
     return carsExists;
   }
